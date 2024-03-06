f5d40ca1c33be62ecd7c86e6ab30469d2759c16e
Yashvi Desaii <yashvi.desai999@gmail.com>
2024-03-05 17:04:32 -0800

    -established connection to firebase

---
+++ backend/firebase_utils.py
@@ -0,0 +1,9 @@
+from flask import Flask
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+
+cred = credentials.Certificate('credentials.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
--- backend/routes/NewPoliceReport.py
+++ backend/routes/NewPoliceReport.py
@@ -1,8 +1,12 @@
-from flask import Flask
-from flask import Blueprint
+from flask import Flask, jsonify, request, Blueprint
+from firebase_admin import firestore
+from firebase_utils import db
 
 NewPoliceReport = Blueprint('NewPoliceReport', __name__)
 
-@NewPoliceReport.route('/new_report')
+@NewPoliceReport.route('/new_report', methods=['POST'])
 def new_police_report():
-    return "This would be the webpage which allows the law enforecement agents to fill out a police report and upload the relevant images to the database."
+    content = request.json
+    doc_ref = db.collection('your_collection_name').document('your_document_id')
+    doc_ref.set(content)
+    return jsonify({"success": True}), 200
